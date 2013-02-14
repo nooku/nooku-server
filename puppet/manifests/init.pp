1aa009d955d769de98c1ef90da0271b9f68631d5
Gergo Erdosi <gergo@timble.net>
2013-02-14 23:26:05 +0100

    Add defaults to Exec and Package. Include PHP module. re #260

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,5 +1,14 @@
+Exec {
+  path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ]
+}
+
+Package {
+  require => Exec['apt_update']
+}
+
 import 'nodes/*'
 
 node server {
   include nginx
+  include php
 }
