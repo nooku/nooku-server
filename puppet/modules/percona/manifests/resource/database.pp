5b5bf705ea1d66737c7f7ebaa3e079c2c4c32647
Gergo Erdosi <gergo@timble.net>
2013-03-08 00:30:44 +0100

    Add database and import resources. re #267

---
+++ puppet/modules/percona/manifests/resource/database.pp
@@ -0,0 +1,13 @@
+define percona::resource::database() {
+  exec { "drop-database-${name}":
+    command   => "mysql --user='root' --password='root' --execute 'DROP DATABASE IF EXISTS `${name}`;'",
+    logoutput => on_failure,
+    require   => $require,
+  }
+
+  exec { "create-database-${name}":
+    command   => "mysql --user='root' --password='root' --execute 'CREATE DATABASE `${name}`;'",
+    logoutput => on_failure,
+    require   => Exec["drop-database-${name}"],
+  }
+}
