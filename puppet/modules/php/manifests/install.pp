71d30a46680d3fb66cd80cebdfd11250c2e1b0d7
Gergo Erdosi <gergo@timble.net>
2013-03-16 22:56:22 +0100

    Refactor PHP module, fix configuration bugs. re #260

--- puppet/modules/php/manifests/install.pp
+++ puppet/modules/php/manifests/install.pp
@@ -1,308 +1,154 @@
 class php::install {
-  define php::install::source {
-    include php::params
+  $packages = [
+    'build-essential',
+    'autoconf',
+    'libxml2-dev',
+    'libpcre3-dev',
+    'libbz2-dev',
+    'libcurl4-openssl-dev',
+    'libjpeg-dev',
+    'libpng12-dev',
+    'libxpm-dev',
+    'libfreetype6-dev',
+    'libmysqlclient-dev',
+    'libt1-dev',
+    'libgd2-xpm-dev',
+    'libgmp-dev',
+    'libsasl2-dev',
+    'libmhash-dev',
+    'freetds-dev',
+    'libpspell-dev',
+    'libsnmp-dev',
+    'libtidy-dev',
+    'libxslt1-dev',
+    'libmcrypt-dev'
+  ]
 
-    $version = $name
-
-    $short_version = $version ? {
-      /^5\.3/ => '53',
-      /^5\.4/ => '54',
-    }
-
-    $url = "http://www.php.net/get/php-${version}.tar.gz/from/this/mirror"
-
-    $configure = $version ? {
-      /^5\.3/ => $php::params::configure_53,
-      /^5\.4/ => $php::params::configure_54,
-    }
-
-    $xdebug = $version ? {
-      /^5\.3/ => '/usr/local/php53/lib/php/extensions/no-debug-non-zts-20090626/xdebug.so',
-      /^5\.4/ => '/usr/local/php54/lib/php/extensions/no-debug-non-zts-20100525/xdebug.so',
-    }
-
-    if ! defined(Package['build-essential']) {
-      package { 'build-essential':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['autoconf']) {
-      package { 'autoconf':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libxml2-dev']) {
-      package { 'libxml2-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libpcre3-dev']) {
-      package { 'libpcre3-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libbz2-dev']) {
-      package { 'libbz2-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libcurl4-openssl-dev']) {
-      package { 'libcurl4-openssl-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libjpeg-dev']) {
-      package { 'libjpeg-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libpng12-dev']) {
-      package { 'libpng12-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libxpm-dev']) {
-      package { 'libxpm-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libfreetype6-dev']) {
-      package { 'libfreetype6-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libmysqlclient-dev']) {
-      package { 'libmysqlclient-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libt1-dev']) {
-      package { 'libt1-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libgd2-xpm-dev']) {
-      package { 'libgd2-xpm-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libgmp-dev']) {
-      package { 'libgmp-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libsasl2-dev']) {
-      package { 'libsasl2-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libmhash-dev']) {
-      package { 'libmhash-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['freetds-dev']) {
-      package { 'freetds-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libpspell-dev']) {
-      package { 'libpspell-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libsnmp-dev']) {
-      package { 'libsnmp-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libtidy-dev']) {
-      package { 'libtidy-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libxslt1-dev']) {
-      package { 'libxslt1-dev':
-        ensure => present,
-      }
-    }
-
-    if ! defined(Package['libmcrypt-dev']) {
-      package { 'libmcrypt-dev':
-        ensure => present,
-      }
-    }
-
-    exec { "download-${version}":
-      cwd       => '/tmp',
-      command   => "wget -O php-${version}.tar.gz ${url}",
-      creates   => "/usr/local/php${short_version}",
-      timeout   => 3600,
-      notify    => Exec["extract-${version}"],
-      logoutput => 'on_failure',
-    }
-
-    exec { "extract-${version}":
-      cwd         => '/tmp',
-      command     => "tar xzvpf php-${version}.tar.gz -C /usr/src",
-      unless      => "ls /usr/src/php-${version}",
-      logoutput   => on_failure,
-      refreshonly => true,
-      notify      => Exec["configure-${version}"],
-      require     => Exec["download-${version}"],
-    }
-
-    exec { "configure-${version}":
-      cwd         => "/usr/src/php-${version}",
-      command     => "./configure $configure",
-      path        => [ "/usr/src/php-${version}", '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-      timeout     => 0,
-      refreshonly => true,
-      logoutput   => on_failure,
-      notify      => Exec["make-${version}"],
-      require     => [ Exec["extract-${version}"], Package['build-essential'], Package['autoconf'], Package['libxml2-dev'], Package['libpcre3-dev'], Package['libbz2-dev'], Package['libcurl4-openssl-dev'], Package['libjpeg-dev'], Package['libpng12-dev'], Package['libxpm-dev'], Package['libfreetype6-dev'], Package['libmysqlclient-dev'], Package['libt1-dev'], Package['libgd2-xpm-dev'], Package['libgmp-dev'], Package['libsasl2-dev'], Package['libmhash-dev'], Package['freetds-dev'], Package['libpspell-dev'], Package['libsnmp-dev'], Package['libtidy-dev'], Package['libxslt1-dev'], Package['libmcrypt-dev'] ],
-    }
-
-    exec { "make-${version}":
-      cwd         => "/usr/src/php-${version}",
-      command     => "make",
-      timeout     => 0,
-      refreshonly => true,
-      logoutput   => on_failure,
-      notify      => Exec["make-install-${version}"],
-      require     => Exec["configure-${version}"],
-    }
-
-    exec { "make-install-${version}":
-      cwd         => "/usr/src/php-${version}",
-      command     => "make install",
-      timeout     => 0,
-      refreshonly => true,
-      logoutput   => on_failure,
-      require     => Exec["make-${version}"],
-    }
-
-    exec { "pear-channel-update-${version}":
-      cwd         => "/usr/local/php${short_version}/bin",
-      command     => 'pear channel-update pear.php.net',
-      path        => [ "/usr/local/php${short_version}/bin", '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-      timeout     => 0,
-      refreshonly => true,
-      logoutput   => on_failure,
-      notify      => Exec["pear-upgrade-all-${version}"],
-      require     => Exec["make-install-${version}"],
-    }
-
-    exec { "pear-upgrade-all-${version}":
-      cwd         => "/usr/local/php${short_version}/bin",
-      command     => 'pear upgrade-all',
-      path        => [ "/usr/local/php${short_version}/bin", '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-      timeout     => 0,
-      refreshonly => true,
-      logoutput   => on_failure,
-      require     => Exec["pear-channel-update-${version}"],
-    }
-
-    exec { "pear-discover-phpunit-${version}":
-      cwd       => "/usr/local/php${short_version}/bin",
-      command   => 'pear channel-discover pear.phpunit.de',
-      path      => [ "/usr/local/php${short_version}/bin", '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-      timeout   => 0,
-      logoutput => on_failure,
-      creates   => "/usr/local/php${short_version}/bin/phpunit",
-      require   => Exec["pear-upgrade-all-${version}"],
-    }
-
-    exec { "pear-discover-symfony-${version}":
-      cwd       => "/usr/local/php${short_version}/bin",
-      command   => 'pear channel-discover pear.symfony.com',
-      path      => [ "/usr/local/php${short_version}/bin", '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-      timeout   => 0,
-      logoutput => on_failure,
-      creates   => "/usr/local/php${short_version}/bin/phpunit",
-      require   => Exec["pear-upgrade-all-${version}"],
-    }
-
-    exec { "pear-discover-ez-${version}":
-      cwd       => "/usr/local/php${short_version}/bin",
-      command   => 'pear channel-discover components.ez.no',
-      path      => [ "/usr/local/php${short_version}/bin", '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-      timeout   => 0,
-      logoutput => on_failure,
-      creates   => "/usr/local/php${short_version}/bin/phpunit",
-      require   => Exec["pear-upgrade-all-${version}"],
-    }
-
-    exec { "pear-install-phpunit-${version}":
-      cwd         => "/usr/local/php${short_version}/bin",
-      command     => 'pear install phpunit/PHPUnit',
-      path        => [ "/usr/local/php${short_version}/bin", '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-      timeout     => 0,
-      logoutput   => on_failure,
-      creates     => "/usr/local/php${short_version}/bin/phpunit",
-      require     => [ Exec["pear-discover-phpunit-${version}"], Exec["pear-discover-symfony-${version}"], Exec["pear-discover-ez-${version}"] ],
-    }
-
-    exec { "install-composer-${version}":
-      cwd       => "/usr/local/php${short_version}/bin",
-      command   => "php -r \"eval('?>'.file_get_contents('https://getcomposer.org/installer'));\"",
-      path      => [ "/usr/local/php${short_version}/bin", '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-      timeout   => 0,
-      logoutput => on_failure,
-      creates   => "/usr/local/php${short_version}/bin/composer.phar",
-      require   => Exec["make-install-${version}"],
-    }
-
-    exec { "install-php-analyzer-${version}":
-      cwd       => "/usr/local/php${short_version}/bin",
-      command   => "composer create-project scrutinizer/php-analyzer:dev-master",
-      path      => [ "/usr/local/php${short_version}/bin", '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-      timeout   => 0,
-      logoutput => on_failure,
-      require   => Exec["install-composer-${version}"],
-    }
-
-    exec { "install-xdebug-${version}":
-      cwd       => "/usr/local/php${short_version}/bin",
-      command   => "pecl install xdebug",
-      path      => [ "/usr/local/php${short_version}/bin", '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-      timeout   => 0,
-      logoutput => on_failure,
-      creates   => "${xdebug}",
-      require   => Exec["make-install-${version}"],
-    }
-
-    exec { "install-taint-${version}":
-      cwd       => "/usr/local/php${short_version}/bin",
-      command   => "pecl install taint",
-      path      => [ "/usr/local/php${short_version}/bin", '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ],
-      timeout   => 0,
-      logoutput => on_failure,
-      creates   => "${xdebug}",
-      require   => Exec["make-install-${version}"],
-    }
+  package { $packages:
+    ensure => present,
   }
 
-  php::install::source { '5.3.22': }
-  php::install::source { '5.4.12': }
+  php::install::source { ['5.3.22', '5.4.12']:
+    packages => $packages,
+  }
+
+  $php_bin = '/usr/local/php54/bin'
+  $require = Anchor['php::install::source::5.4.12']
+
+  file { '/usr/bin/php':
+    ensure  => link,
+    target  => "${php_bin}/php",
+    require => $require,
+  }
+
+  file { '/usr/bin/php-config':
+    ensure  => link,
+    target  => "${php_bin}/php-config",
+    require => $require,
+  }
+
+  file { '/usr/bin/pear':
+    ensure  => link,
+    target  => "${php_bin}/pear",
+    require => $require,
+  }
+
+  file { '/usr/bin/pecl':
+    ensure  => link,
+    target  => "${php_bin}/pecl",
+    require => $require,
+  }
+
+  file { '/usr/bin/phar':
+    ensure  => link,
+    target  => "${php_bin}/phar.phar",
+    require => $require,
+  }
+
+  file { '/usr/bin/phpize':
+    ensure  => link,
+    target  => "${php_bin}/phpize",
+    require => $require,
+  }
+
+  exec { 'pear-channel-update':
+    command     => 'pear channel-update pear.php.net',
+    timeout     => 0,
+    logoutput   => on_failure,
+    require     => File['/usr/bin/php', '/usr/bin/pear'],
+  }
+
+  exec { 'pear-upgrade-all':
+    command     => 'pear upgrade-all',
+    timeout     => 0,
+    require     => File['/usr/bin/php', '/usr/bin/pear'],
+  }
+
+  exec { 'pear-discover-phpunit':
+    command   => 'pear channel-discover pear.phpunit.de',
+    creates   => "${php_bin}/phpunit",
+    timeout   => 0,
+    logoutput => on_failure,
+    require   => File['/usr/bin/php', '/usr/bin/pear'],
+  }
+
+  exec { 'pear-discover-symfony':
+    command   => 'pear channel-discover pear.symfony.com',
+    creates   => "${php_bin}/phpunit",
+    timeout   => 0,
+    logoutput => on_failure,
+    require   => Exec['pear-upgrade-all'],
+  }
+
+  exec { 'pear-discover-ez':
+    command   => 'pear channel-discover components.ez.no',
+    creates   => "${php_bin}/phpunit",
+    timeout   => 0,
+    logoutput => on_failure,
+    require   => File['/usr/bin/php', '/usr/bin/pear'],
+  }
+
+  exec { 'pear-install-phpunit':
+    command     => 'pear install phpunit/PHPUnit',
+    creates     => "${php_bin}/phpunit",
+    timeout     => 0,
+    logoutput   => on_failure,
+    require     => Exec['pear-discover-phpunit', 'pear-discover-symfony', 'pear-discover-ez'],
+  }
+
+  file { '/usr/bin/phpunit':
+    ensure  => link,
+    target  => "${php_bin}/phpunit",
+    require => Exec['pear-install-phpunit'],
+  }
+
+  exec { 'install-composer':
+    cwd       => "${php_bin}",
+    command   => 'curl -s https://getcomposer.org/installer | php',
+    creates   => "${php_bin}/composer.phar",
+    timeout   => 0,
+    logoutput => on_failure,
+    require   => File['/usr/bin/php'],
+  }
+
+  file { '/usr/bin/composer':
+    ensure  => link,
+    target  => "${php_bin}/composer.phar",
+    require => Exec['install-composer'],
+  }
+
+  exec { 'install-php-analyzer':
+    cwd       => '/usr/local',
+    command   => 'composer create-project scrutinizer/php-analyzer:dev-master',
+    creates   => '/usr/local/php-analyzer',
+    timeout   => 0,
+    logoutput => on_failure,
+    require   => File['/usr/bin/composer'],
+  }
+
+  file { '/usr/bin/phpalizer':
+    ensure  => link,
+    target  => '/usr/local/php-analyzer/bin/phpalizer',
+    require => Exec['install-php-analyzer'],
+  }
 }
--- puppet/modules/php/manifests/params.pp
+++ puppet/modules/php/manifests/params.pp
@@ -1,4 +1,7 @@
 class php::params {
+  $xdebug_53 = '/usr/local/php53/lib/php/extensions/no-debug-non-zts-20090626/xdebug.so'
+  $xdebug_54 = '/usr/local/php54/lib/php/extensions/no-debug-non-zts-20100525/xdebug.so'
+
   $conf_dir_53 = '/etc/php53'
   $conf_dir_54 = '/etc/php54'
 
@@ -6,7 +9,7 @@ class php::params {
     --enable-fpm\
     --prefix=/usr/local/php53\
     --sysconfdir=/etc/php53\
-    --with-config-file-path=/etc/php53/cli\
+    --with-config-file-path=/etc/php53\
     --with-config-file-scan-dir=/etc/php53/conf.d\
     --enable-bcmath\
     --enable-calendar\
@@ -44,7 +47,7 @@ class php::params {
     --enable-fpm\
     --prefix=/usr/local/php54\
     --sysconfdir=/etc/php54\
-    --with-config-file-path=/etc/php54/cli\
+    --with-config-file-path=/etc/php54\
     --with-config-file-scan-dir=/etc/php54/conf.d\
     --enable-bcmath\
     --enable-calendar\
