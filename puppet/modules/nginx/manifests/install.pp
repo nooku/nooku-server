15d8016dbf4406f9a7ea553d39c51b6cfb9e7d1b
Gergo Erdosi <gergo@timble.net>
2013-03-31 19:51:59 +0200

    Refactor Nginx module to use FastCGI instead of FPM. re #276

--- puppet/modules/nginx/manifests/install.pp
+++ puppet/modules/nginx/manifests/install.pp
@@ -1,6 +1,12 @@
 class nginx::install {
   include apt
 
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0755',
+  }
+
   apt::source { 'nginx':
     location   => 'http://nginx.org/packages/ubuntu/',
     repos      => 'nginx',
@@ -11,5 +17,35 @@ class nginx::install {
   package { 'nginx':
     ensure  => present,
     require => Anchor['apt::source::nginx'],
+  }
+
+  package { 'spawn-fcgi':
+    ensure => present,
+  }
+
+  file { '/usr/bin/php-fastcgi-5.3':
+    ensure => file,
+    source => 'puppet:///modules/nginx/usr/bin/php-fastcgi-5.3',
+  }
+
+  file { '/usr/bin/php-fastcgi-5.4':
+    ensure => file,
+    source => 'puppet:///modules/nginx/usr/bin/php-fastcgi-5.4',
+  }
+
+  file { '/etc/init.d/php-fastcgi-5.3':
+    ensure => file,
+    source => 'puppet:///modules/nginx/etc/init.d/php-fastcgi-5.3',
+  }
+
+  file { '/etc/init.d/php-fastcgi-5.4':
+    ensure => file,
+    source => 'puppet:///modules/nginx/etc/init.d/php-fastcgi-5.4',
+  }
+
+  file { '/var/run/php-fastcgi':
+    ensure => directory,
+    owner  => 'www-data',
+    group  => 'www-data',
   }
 }
--- puppet/modules/nginx/manifests/service.pp
+++ puppet/modules/nginx/manifests/service.pp
@@ -1,7 +1,23 @@
 class nginx::service {
-  $require = Class['nginx::config']
+  $require = [ Class['nginx::install'], Class['nginx::config'] ]
 
   service { 'nginx':
+    ensure     => running,
+    hasstatus  => true,
+    hasrestart => true,
+    enable     => true,
+    require    => $require,
+  }
+
+  service { 'php-fastcgi-5.3':
+    ensure     => running,
+    hasstatus  => true,
+    hasrestart => true,
+    enable     => true,
+    require    => $require,
+  }
+
+  service { 'php-fastcgi-5.4':
     ensure     => running,
     hasstatus  => true,
     hasrestart => true,
