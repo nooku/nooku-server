ab7e3aab415516dc698471f4ff5e0b6c276d8d5d
Gergo Erdosi <gergo@timble.net>
2013-03-31 19:51:29 +0200

    Refactor PHP module to use phpfarm. re #276

--- puppet/modules/php/manifests/service.pp
+++ puppet/modules/php/manifests/service.pp
@@ -1,11 +1,3 @@
 class php::service {
   $require = Class['php::config']
-
-  service { ['php53-fpm', 'php54-fpm']:
-    ensure     => running,
-    hasstatus  => false,
-    hasrestart => true,
-    enable     => false,
-    require    => $require,
-  }
 }
