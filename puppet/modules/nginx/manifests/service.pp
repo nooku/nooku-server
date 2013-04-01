34dee1c98ef45ec236ec4a4771a2757eaa88e080
Gergo Erdosi <gergo@timble.net>
2013-04-01 17:44:18 +0200

    Revert "Refactor Nginx module to use FastCGI instead of FPM. re #276"
    This reverts commit 15d8016dbf4406f9a7ea553d39c51b6cfb9e7d1b.

--- puppet/modules/nginx/manifests/service.pp
+++ puppet/modules/nginx/manifests/service.pp
@@ -1,23 +1,7 @@
 class nginx::service {
-  $require = [ Class['nginx::install'], Class['nginx::config'] ]
+  $require = Class['nginx::config']
 
   service { 'nginx':
-    ensure     => running,
-    hasstatus  => true,
-    hasrestart => true,
-    enable     => true,
-    require    => $require,
-  }
-
-  service { 'php-fastcgi-5.3':
-    ensure     => running,
-    hasstatus  => true,
-    hasrestart => true,
-    enable     => true,
-    require    => $require,
-  }
-
-  service { 'php-fastcgi-5.4':
     ensure     => running,
     hasstatus  => true,
     hasrestart => true,
