64f891a8cf76b7fde62db7632067115abfaf2581
Gergo Erdosi <gergo@timble.net>
2013-04-01 17:44:25 +0200

    Revert "Refactor PHP module to use phpfarm. re #276"
    This reverts commit ab7e3aab415516dc698471f4ff5e0b6c276d8d5d.

--- puppet/modules/php/manifests/config.pp
+++ puppet/modules/php/manifests/config.pp
@@ -1,47 +1,82 @@
 class php::config {
-  $require = Class['php::install::php']
+  define php::config::source {
+    $require = Class['php::install']
 
-  file { '/etc/php-5.3':
-    ensure  => directory,
-    require => $require,
+    File {
+      owner => 'root',
+      group => 'root',
+      mode  => '0644',
+    }
+
+    $version = $name
+
+    case $version {
+      '5.3': {
+        include php::params::53
+
+        $short_version = '53'
+        $conf_dir      = $php::params::53::conf_dir
+      }
+      '5.4': {
+        include php::params::54
+
+        $short_version = '54'
+        $conf_dir      = $php::params::54::conf_dir
+      }
+    }
+
+    file { "${conf_dir}":
+      ensure  => directory,
+      require => $require,
+    }
+
+    file { "${conf_dir}/conf.d":
+      ensure  => directory,
+      require => File["${conf_dir}"],
+    }
+
+    file { "${conf_dir}/php.ini":
+      ensure  => file,
+      content => template("php/${version}/php.ini.erb"),
+      notify  => Class['php::service'],
+      require => File["${conf_dir}"],
+    }
+
+    file { "${conf_dir}/php-fpm.conf.default":
+      ensure  => absent,
+      require => File["${conf_dir}"],
+    }
+
+    file { "${conf_dir}/php-fpm.conf":
+      ensure  => file,
+      content => template("php/${version}/php-fpm.conf.erb"),
+      notify  => Class['php::service'],
+      require => File["${conf_dir}"],
+    }
+
+    file { "${conf_dir}/conf.d/xdebug.ini":
+      ensure  => file,
+      content => template("php/${version}/xdebug.ini.erb"),
+      notify  => Class['php::service'],
+      require => File["${conf_dir}/conf.d"],
+    }
+
+    file { "${conf_dir}/conf.d/yaml.ini":
+      ensure  => file,
+      content => template("php/${version}/yaml.ini.erb"),
+      notify  => Class['php::service'],
+      require => File["${conf_dir}/conf.d"],
+    }
+
+    file { "/etc/init.d/php${short_version}-fpm":
+      ensure  => file,
+      content => template("php/${version}/php-fpm.init.erb"),
+      mode    => 0744,
+      notify  => Class['php::service'],
+      require => $require,
+    }
   }
 
-  file { '/etc/php-5.3/conf.d':
-    ensure  => directory,
-    require => File['/etc/php-5.3'],
-  }
-
-  file { "/etc/php-5.3/php.ini":
-    ensure  => file,
-    source  => 'puppet:///modules/php/etc/php-5.3/php.ini',
-    require => File['/etc/php-5.3'],
-  }
-
-  file { "/etc/php-5.3/conf.d/xdebug.ini":
-    ensure  => file,
-    source  => 'puppet:///modules/php/etc/php-5.3/conf.d/xdebug.ini',
-    require => File['/etc/php-5.3/conf.d'],
-  }
-
-  file { '/etc/php-5.4':
-    ensure  => directory,
-    require => $require,
-  }
-
-  file { '/etc/php-5.4/conf.d':
-    ensure  => directory,
-    require => File['/etc/php-5.4'],
-  }
-
-  file { "/etc/php-5.4/php.ini":
-    ensure  => file,
-    source  => 'puppet:///modules/php/etc/php-5.4/php.ini',
-    require => File['/etc/php-5.4'],
-  }
-
-  file { "/etc/php-5.4/conf.d/xdebug.ini":
-    ensure  => file,
-    source  => 'puppet:///modules/php/etc/php-5.4/conf.d/xdebug.ini',
-    require => File['/etc/php-5.4/conf.d'],
-  }
+  php::config::source { '5.3': }
+  php::config::source { '5.4': }
 }
