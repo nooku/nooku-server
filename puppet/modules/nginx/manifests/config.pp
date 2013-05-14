9616d75724372b15fa0c16299bad0c4796fe96e5
Gergo Erdosi <gergo@timble.net>
2013-05-14 23:29:07 +0200

    Add config files. re #1

--- puppet/modules/nginx/manifests/config.pp
+++ puppet/modules/nginx/manifests/config.pp
@@ -29,6 +29,48 @@ class nginx::config {
     require => File["${nginx::params::conf_dir}"],
   }
 
+  file { "${nginx::params::conf_dir}/fastcgi_params":
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/etc/nginx/fastcgi_params',
+    require => File["${nginx::params::conf_dir}"],
+  }
+
+  file { "${nginx::params::conf_dir}/koi-utf":
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/etc/nginx/koi-utf',
+    require => File["${nginx::params::conf_dir}"],
+  }
+
+  file { "${nginx::params::conf_dir}/koi-win":
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/etc/nginx/koi-win',
+    require => File["${nginx::params::conf_dir}"],
+  }
+
+  file { "${nginx::params::conf_dir}/mime.types":
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/etc/nginx/mime.types',
+    require => File["${nginx::params::conf_dir}"],
+  }
+
+  file { "${nginx::params::conf_dir}/scgi_params":
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/etc/nginx/scgi_params',
+    require => File["${nginx::params::conf_dir}"],
+  }
+
+  file { "${nginx::params::conf_dir}/uwsgi_params":
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/etc/nginx/uwsgi_params',
+    require => File["${nginx::params::conf_dir}"],
+  }
+
+  file { "${nginx::params::conf_dir}/win-utf":
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/etc/nginx/win-utf',
+    require => File["${nginx::params::conf_dir}"],
+  }
+
   file { "${nginx::params::conf_dir}/nginx.conf":
     ensure  => file,
     content => template('nginx/nginx.conf.erb'),
@@ -41,5 +83,10 @@ class nginx::config {
     content => template('nginx/nooku.inc.erb'),
     notify  => Class['nginx::service'],
     require => File["${nginx::params::conf_dir}"],
+  }
+
+  file { '/etc/init.d/nginx':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/etc/init.d/nginx',
   }
 }
